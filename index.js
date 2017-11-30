c7523df5de7156ae8c589a9448d3fb0ff09d33c7
Ramon Imbao <ramonimbao@gmail.com>
2017-11-30 18:00:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,14 @@
  */
 
 module.exports = require('./src/SpoilerBot');
+
+'use strict';
+
+const SpoilerBot = require('discord-spoiler-bot');
+
+let config = {
+    token: 'Mzg1ODM0NzQxMzcyNTUxMTY4.DQHIHw.hK7gRJlvSYzqlkl89B5Aih_6GR0',
+};
+
+let bot = new SpoilerBot(config);
+bot.connect();
