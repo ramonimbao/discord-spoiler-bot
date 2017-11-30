e2259067768b61ce15bdb5fbc8562a19739eb041
Ramon Imbao <ramonimbao@gmail.com>
2017-11-30 18:09:34 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const SpoilerBot = require('discord-spoiler-bot');
+
+let config = {
+    token: 'Mzg1ODM0NzQxMzcyNTUxMTY4.DQHIHw.hK7gRJlvSYzqlkl89B5Aih_6GR0',
+};
+
+let bot = new SpoilerBot(config);
+bot.connect();
